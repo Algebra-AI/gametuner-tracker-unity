4239f603efb1cb5990924169f7981b2d472e1aba
Djordje <djordje@twodesperados.com>
2021-11-01 14:13:58 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,136 @@
+# Unity Analytics for Snowplow
+
+[![early-release]][tracker-classificiation]
+[![Build][github-image]][github-action]
+[![Release][release-image]][releases]
+[![License][license-image]][license]
+
+## Overview
+
+Add analytics to your Unity games and apps with the **[Snowplow][snowplow]** event tracker for **[Unity][unity]**.
+
+With this tracker you can collect event data from your Unity-based applications, games or frameworks.
+
+## Quick start
+
+### Requirements
+
+* SnowplowTracker requires Unity 2018.1+ and .NET Standard 2.0 Api Compatibility Level.
+* SnowplowTracker requires Newtonsoft.Json. This is [bundled with Unity 2020.1+][unity-newtonsoftjson] or [Newtonsoft.Json-for-Unity][newtonsoftjson-for-unity] can be used for earlier versions.
+* SnowplowTracker supports both Mono and IL2CPP scripting backends.
+* SnowplowTracker.Demo and SnowplowTracker.Tests are Unity 2020.3.16f1 (LTS) projects.
+
+### Installation
+
+* Download the `unitypackage` from [GitHub Releases][releases]. Drop into your Unity Editor.
+* If Newtonsoft.Json.dll isn't available, install [Newtonsoft.Json-for-Unity][newtonsoftjson-for-unity].
+
+## Find out more
+
+| Technical Docs                 | Setup Guide               | Roadmap                | Contributing                     |
+|--------------------------------|---------------------------|------------------------|----------------------------------|
+| ![i1][techdocs-image]          | ![i2][setup-image]        | ![i3][roadmap-image]   | ![i4][contributing-image]        |
+| **[Technical Docs][techdocs]** | **[Setup Guide][setup]**  | **[Roadmap][roadmap]** | **[Contributing](CONTRIBUTING.md)** |
+
+## Maintainer Quick start
+
+### Building
+
+Assuming git, **[Vagrant][vagrant-install]** and **[VirtualBox][virtualbox-install]** installed:
+
+```bash
+ host$ git clone https://github.com/snowplow/snowplow-unity-tracker.git
+ host$ cd snowplow-unity-tracker
+```
+
+### Development
+
+To work on the Tracker:
+
+* Open the following file in your IDE of choice (Visual Studio, Visual Studio for Mac or JetBrains Rider): `snowplow-unity-tracker/SnowplowTracker/SnowplowTracker.sln`
+* This solution file will open the `SnowplowTracker` library in your editor.
+* Build files will output to bin/Debug or bin/Release depending on your configuration.
+* Built DLLs will also be copied on successful builds to the Demo and Test projects Assets folders.
+
+### Setting up a Test Endpoint
+
+```bash
+host$ vagrant up && vagrant ssh
+guest$ cd /vagrant
+guest$ mb &
+guest$ curl -X POST -d @/vagrant/Resources/imposter.json http://localhost:2525/imposters
+```
+
+The test collector to use: `http://localhost:4545`
+
+Now open Mountebank in your browser (on host is fine):
+* **[http://localhost:2525](http://localhost:2525)**
+
+### Testing Framework
+
+Requires: Unity 2018.4.13f1 (LTS)
+
+* Open `snowplow-unity-tracker/SnowplowTracker.Tests` in the Unity Editor
+* Build the SnowplowTracker solution (desribed above) if you have made any changes to the Tracker. This will copy the DLLs to Demo and Tests asset folders.
+* Ensure the Mountebank Test Collector is up and running (desribed above)
+* Open the Test Runner `Window` -> `General` -> `Test Runner` and open `EditMode`
+* Click `Run All` to run all tests
+
+Please note that all Unit Tests are written with **[NUnit][nunit]**.
+
+### Running the Snowplow Demo Game
+
+Requires: Unity 2018.4.13f1 (LTS)
+
+To open the Demo Game simply add the following folder within Unity Hub, `snowplow-unity-tracker/SnowplowTracker.Demo`, you can then build and run it yourself.
+
+Tested Platforms:
+
+* Windows
+* Mac OSX
+* Linux
+* iOS
+* Android
+
+## Copyright and license
+
+The Snowplow Unity Tracker is copyright 2015-2021 Snowplow Analytics Ltd.
+
+Licensed under the **[Apache License, Version 2.0][license]** (the "License");
+you may not use this software except in compliance with the License.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+[snowplow]: https://snowplowanalytics.com
+[unity]: https://unity3d.com/
+[nunit]: http://www.nunit.org/
+[unity-newtonsoftjson]: https://docs.unity3d.com/Packages/com.unity.nuget.newtonsoft-json@2.0
+[newtonsoftjson-for-unity]: https://github.com/jilleJr/Newtonsoft.Json-for-Unity
+
+[vagrant-install]: https://docs.vagrantup.com/v2/installation
+[virtualbox-install]: https://www.virtualbox.org/wiki/Downloads
+
+[release-image]: https://img.shields.io/github/v/release/snowplow/snowplow-unity-tracker
+[releases]: https://github.com/snowplow/snowplow-unity-tracker/releases
+
+[license-image]: https://img.shields.io/github/license/snowplow/snowplow-unity-tracker
+[license]: https://www.apache.org/licenses/LICENSE-2.0
+
+[github-image]: https://github.com/snowplow/snowplow-unity-tracker/actions/workflows/build.yml/badge.svg
+[github-action]: https://github.com/snowplow/snowplow-unity-tracker/actions/workflows/build.yml
+
+[techdocs-image]: https://d3i6fms1cm1j0i.cloudfront.net/github/images/techdocs.png
+[setup-image]: https://d3i6fms1cm1j0i.cloudfront.net/github/images/setup.png
+[roadmap-image]: https://d3i6fms1cm1j0i.cloudfront.net/github/images/roadmap.png
+[contributing-image]: https://d3i6fms1cm1j0i.cloudfront.net/github/images/contributing.png
+
+[techdocs]: https://docs.snowplowanalytics.com/docs/collecting-data/collecting-from-own-applications/unity-tracker/
+[setup]: https://docs.snowplowanalytics.com/docs/collecting-data/collecting-from-own-applications/unity-tracker/setup/
+[roadmap]: https://github.com/snowplow/snowplow/projects/7
+
+[tracker-classificiation]: https://docs.snowplowanalytics.com/docs/collecting-data/collecting-from-own-applications/tracker-maintenance-classification/
+[early-release]: https://img.shields.io/static/v1?style=flat&label=Snowplow&message=Early%20Release&color=014477&labelColor=9ba0aa&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAeFBMVEVMaXGXANeYANeXANZbAJmXANeUANSQAM+XANeMAMpaAJhZAJeZANiXANaXANaOAM2WANVnAKWXANZ9ALtmAKVaAJmXANZaAJlXAJZdAJxaAJlZAJdbAJlbAJmQAM+UANKZANhhAJ+EAL+BAL9oAKZnAKVjAKF1ALNBd8J1AAAAKHRSTlMAa1hWXyteBTQJIEwRgUh2JjJon21wcBgNfmc+JlOBQjwezWF2l5dXzkW3/wAAAHpJREFUeNokhQOCA1EAxTL85hi7dXv/E5YPCYBq5DeN4pcqV1XbtW/xTVMIMAZE0cBHEaZhBmIQwCFofeprPUHqjmD/+7peztd62dWQRkvrQayXkn01f/gWp2CrxfjY7rcZ5V7DEMDQgmEozFpZqLUYDsNwOqbnMLwPAJEwCopZxKttAAAAAElFTkSuQmCC 
---
+++ SnowplowTracker/SnowplowTracker/Emitters/AbstractEmitter.cs
@@ -0,0 +1,392 @@
+/*
+ * AbstractEmitter.cs
+ * SnowplowTracker.Emitters
+ * 
+ * Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ * 
+ * Authors: Joshua Beemster, Paul Boocock
+ * Copyright: Copyright (c) 2015-2019 Snowplow Analytics Ltd
+ * License: Apache License Version 2.0
+ */
+
+using System;
+using System.Collections.Generic;
+using SnowplowTracker.Payloads;
+using SnowplowTracker.Enums;
+using SnowplowTracker.Storage;
+using SnowplowTracker.Collections;
+using SnowplowTracker.Requests;
+using System.Net.Http;
+using System.Text;
+
+namespace SnowplowTracker.Emitters
+{
+    public abstract class AbstractEmitter : IEmitter
+    {
+        protected int POST_WRAPPER_BYTES = 88; // "schema":"iglu:com.snowplowanalytics.snowplow/payload_data/jsonschema/1-0-3","data":[]
+        protected int POST_STM_BYTES = 22;     // "stm":"1443452851000",
+        protected int FAIL_INTERVAL = 10000;  // If all events failed to send
+
+        protected string endpoint;
+        protected Uri collectorUri;
+        protected HttpProtocol httpProtocol;
+        protected Enums.HttpMethod httpMethod;
+        protected int sendLimit;
+        protected long byteLimitGet;
+        protected long byteLimitPost;
+        protected IStore eventStore;
+
+        /// <summary>
+        /// Adds an event payload to the database.
+        /// </summary>
+        /// <param name="payload">Payload.</param>
+        public abstract void Add(TrackerPayload payload);
+
+        /// <summary>
+        /// Starts the Emitter and the Event Consumer.
+        /// - EmitLoop will send all events in the database and then
+        ///   wait for the event consumer to signal that there is work.
+        /// - The event consumer waits for its queue to be signalled and
+        ///   will then add an event to the database; after which it 
+        ///   signals the emitloop of work to be done.
+        /// </summary>
+        public abstract void Start();
+
+        /// <summary>
+        /// Stops the Emitter and the Event Consumer.
+        /// </summary>
+        public abstract void Stop();
+
+        /// <summary>
+        /// Gets whether the emitter is currently sending.
+        /// </summary>
+        /// <returns><c>true</c>, if emitter is sending, <c>false</c> otherwise.</returns>
+        public abstract bool IsSending();
+
+        // --- Event Senders
+
+        /// <summary>
+        /// From a range of event rows will construct a list of RequestResult objects
+        /// </summary>
+        /// <returns>The results of sending all requests</returns>
+        /// <param name="eventRows">Event rows from the database</param>
+        protected List<RequestResult> SendRequests(List<EventRow> eventRows)
+        {
+            ConcurrentQueue<RequestResult> resultQueue = new ConcurrentQueue<RequestResult>();
+            int count;
+
+            if (httpMethod == Enums.HttpMethod.GET)
+            {
+                count = HttpGet(eventRows, resultQueue);
+            }
+            else
+            {
+                count = HttpPost(eventRows, resultQueue);
+            }
+
+            // Wait for the results of each request
+            List<RequestResult> results = new List<RequestResult>();
+            while (count != 0)
+            {
+                results.Add(resultQueue.Dequeue());
+                count--;
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Sends all events as GET requests on background threads
+        /// </summary>
+        /// <returns>The get.</returns>
+        /// <param name="eventRows">Event rows.</param>
+        protected int HttpGet(List<EventRow> eventRows, ConcurrentQueue<RequestResult> resultQueue)
+        {
+            int count = eventRows.Count;
+            try
+            {
+                // Send each row as an individual GET Request
+                foreach (EventRow eRow in eventRows)
+                {
+                    TrackerPayload payload = eRow.GetPayload();
+                    long byteSize = payload.GetByteSize() + POST_STM_BYTES;
+                    bool oversize = byteSize > byteLimitGet;
+                    Log.Debug("Emitter: Sending GET with byte-size: " + byteSize);
+                    new ReadyRequest(
+                        new HttpRequest(Enums.HttpMethod.GET, GetGETRequest(payload.GetDictionary())),
+                        new List<Guid> { eRow.GetRowId() },
+                        oversize,
+                        resultQueue
+                    ).Send();
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Debug("Emitter: caught exception in HTTPGet request: " + e.Message);
+                Log.Debug("Emitter: HTTPGet exception trace: " + e.StackTrace);
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Send all event rows as POST requests on background threads
+        /// </summary>
+        /// <returns>The results of all the requests</returns>
+        /// <param name="eventRows">Event rows.</param>
+        protected int HttpPost(List<EventRow> eventRows, ConcurrentQueue<RequestResult> resultQueue)
+        {
+            int count = 0;
+
+            List<Guid> rowIds = new List<Guid>();
+            List<Dictionary<string, object>> payloadDicts = new List<Dictionary<string, object>>();
+            long totalByteSize = 0;
+            try
+            {
+
+                for (int i = 0; i < eventRows.Count; i++)
+                {
+                    TrackerPayload payload = eventRows[i].GetPayload();
+                    long payloadByteSize = payload.GetByteSize() + POST_STM_BYTES;
+
+                    if ((payloadByteSize + POST_WRAPPER_BYTES) > byteLimitPost)
+                    {
+                        // A single Payload has exceeded the Byte Limit
+                        Log.Debug("Emitter: Single event exceeds byte limit: " + (payloadByteSize + POST_WRAPPER_BYTES) + " is > " + byteLimitPost);
+                        Log.Debug("Sending POST with byte-size: " + (payloadByteSize + POST_WRAPPER_BYTES));
+                        List<Dictionary<string, object>> singlePayloadPost = new List<Dictionary<string, object>> { payload.GetDictionary() };
+                        List<Guid> singlePayloadId = new List<Guid> { eventRows[i].GetRowId() };
+                        new ReadyRequest(new HttpRequest(Enums.HttpMethod.POST, collectorUri, GetPOSTRequest(singlePayloadPost)), singlePayloadId, true, resultQueue).Send();
+                        count++;
+                    }
+                    else if ((totalByteSize + payloadByteSize + POST_WRAPPER_BYTES + (payloadDicts.Count - 1)) > byteLimitPost)
+                    {
+                        Log.Debug("Emitter: Byte limit reached: " + (totalByteSize + payloadByteSize + POST_WRAPPER_BYTES + (payloadDicts.Count - 1)) +
+                                   " is > " + byteLimitPost);
+                        Log.Debug("Emitter: Sending POST with byte-size: " + (totalByteSize + POST_WRAPPER_BYTES + (payloadDicts.Count - 1)));
+                        new ReadyRequest(new HttpRequest(Enums.HttpMethod.POST, collectorUri, GetPOSTRequest(payloadDicts)), rowIds, false, resultQueue).Send();
+                        count++;
+
+                        // Reset collections
+                        payloadDicts = new List<Dictionary<string, object>> { payload.GetDictionary() };
+                        rowIds = new List<Guid> { eventRows[i].GetRowId() };
+                        totalByteSize = payloadByteSize;
+                    }
+                    else
+                    {
+                        payloadDicts.Add(payload.GetDictionary());
+                        rowIds.Add(eventRows[i].GetRowId());
+                        totalByteSize += payloadByteSize;
+                    }
+                }
+
+                if (payloadDicts.Count > 0)
+                {
+                    Log.Debug("Emitter: Sending POST with byte-size: " + (totalByteSize + POST_WRAPPER_BYTES + (payloadDicts.Count - 1)));
+                    new ReadyRequest(new HttpRequest(Enums.HttpMethod.POST, collectorUri, GetPOSTRequest(payloadDicts)), rowIds, false, resultQueue).Send();
+                    count++;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Debug("Emitter: caught exception in HTTPPost request: " + e.Message);
+                Log.Debug("Emitter: HTTPPost exception trace: " + e.StackTrace);
+            }
+            return count;
+        }
+
+        // --- Helpers
+
+        /// <summary>
+        /// Gets a ready request containing a POST
+        /// </summary>
+        /// <returns>The POST request that is already being sent</returns>
+        /// <param name="events">Events to send in the post</param>
+        protected HttpContent GetPOSTRequest(List<Dictionary<string, object>> events)
+        {
+            // Add STM to event
+            AddSentTimeToEvents(events);
+
+            // Build the event
+            SelfDescribingJson sdj = new SelfDescribingJson(Constants.SCHEMA_PAYLOAD_DATA, events);
+
+			// Build the HTTP Content Body
+            HttpContent httpContent = new StringContent(sdj.ToString(), Encoding.UTF8, Constants.POST_CONTENT_TYPE);
+            return httpContent;
+        }
+
+        /// <summary>
+        /// Gets a ready request containing a GET
+        /// </summary>
+        /// <returns>The GET request that is already being sent</returns>
+        /// <param name="eventDict">The event to be converted into a URI</param>
+        protected Uri GetGETRequest(Dictionary<string, object> eventDict)
+        {
+            // Add STM to event
+            eventDict.Add(Constants.SENT_TIMESTAMP, Utils.GetTimestamp().ToString());
+
+            // Build the event
+            return new Uri(collectorUri + Utils.ToQueryString(eventDict));
+        }
+
+        /// <summary>
+        /// Gets the collector URI.
+        /// </summary>
+        /// <returns>The collector URI.</returns>
+        /// <param name="endpoint">Endpoint.</param>
+        /// <param name="protocol">Protocol.</param>
+        /// <param name="method">Method.</param>
+        protected Uri MakeCollectorUri(string endpoint, HttpProtocol protocol, Enums.HttpMethod method)
+        {
+            string path = (method == Enums.HttpMethod.GET) ? Constants.GET_URI_SUFFIX : Constants.POST_URI_SUFFIX;
+            string requestProtocol = (protocol == HttpProtocol.HTTP) ? "http" : "https";
+            return new Uri($"{requestProtocol}://{endpoint}{path}");
+        }
+
+        /// <summary>
+        /// Adds the sent time to a list of event payloads
+        /// </summary>
+        /// <param name="events">The event list to add the stm to</param>
+        protected void AddSentTimeToEvents(List<Dictionary<string, object>> events)
+        {
+            string stm = Utils.GetTimestamp().ToString();
+            foreach (Dictionary<string, object> eventDict in events)
+            {
+                eventDict.Add(Constants.SENT_TIMESTAMP, stm);
+            }
+        }
+
+        // --- Setters
+
+        /// <summary>
+        /// Sets the collector URI.
+        /// </summary>
+        /// <param name="endpoint">Endpoint.</param>
+        public void SetCollectorUri(string endpoint)
+        {
+            this.endpoint = endpoint;
+            collectorUri = MakeCollectorUri(this.endpoint, this.httpProtocol, this.httpMethod);
+        }
+
+        /// <summary>
+        /// Sets the http protocol.
+        /// </summary>
+        /// <param name="httpProtocol">Http protocol.</param>
+        public void SetHttpProtocol(HttpProtocol httpProtocol)
+        {
+            this.httpProtocol = httpProtocol;
+            collectorUri = MakeCollectorUri(this.endpoint, this.httpProtocol, this.httpMethod);
+        }
+
+        /// <summary>
+        /// Sets the http method.
+        /// </summary>
+        /// <param name="httpMethod">Http method.</param>
+        public void SetHttpMethod(Enums.HttpMethod httpMethod)
+        {
+            this.httpMethod = httpMethod;
+            collectorUri = MakeCollectorUri(this.endpoint, this.httpProtocol, this.httpMethod);
+        }
+
+        /// <summary>
+        /// Sets the send limit; this controls how many events are grabbed out of the database at anytime.
+        /// </summary>
+        /// <param name="sendLimit">Send limit.</param>
+        public void SetSendLimit(int sendLimit)
+        {
+            this.sendLimit = sendLimit;
+        }
+
+        /// <summary>
+        /// Sets the byte limit for get requests.
+        /// </summary>
+        /// <param name="byteLimitGet">Byte limit get.</param>
+        public void SetByteLimitGet(long byteLimitGet)
+        {
+            this.byteLimitGet = byteLimitGet;
+        }
+
+        /// <summary>
+        /// Sets the byte limit for post requests.
+        /// </summary>
+        /// <param name="byteLimitPost">Byte limit post.</param>
+        public void SetByteLimitPost(long byteLimitPost)
+        {
+            this.byteLimitPost = byteLimitPost;
+        }
+
+        // --- Getters
+
+        /// <summary>
+        /// Gets the collector URI.
+        /// </summary>
+        /// <returns>The collector URI.</returns>
+        public Uri GetCollectorUri()
+        {
+            return collectorUri;
+        }
+
+        /// <summary>
+        /// Gets the http protocol.
+        /// </summary>
+        /// <returns>The http protocol.</returns>
+        public HttpProtocol GetHttpProtocol()
+        {
+            return httpProtocol;
+        }
+
+        /// <summary>
+        /// Gets the http method.
+        /// </summary>
+        /// <returns>The http method.</returns>
+        public Enums.HttpMethod GetHttpMethod()
+        {
+            return httpMethod;
+        }
+
+        /// <summary>
+        /// Gets the send limit.
+        /// </summary>
+        /// <returns>The send limit.</returns>
+        public int GetSendLimit()
+        {
+            return sendLimit;
+        }
+
+        /// <summary>
+        /// Gets the byte limit for get requests.
+        /// </summary>
+        /// <returns>The byte limit get.</returns>
+        public long GetByteLimitGet()
+        {
+            return byteLimitGet;
+        }
+
+        /// <summary>
+        /// Gets the byte limit for post requests.
+        /// </summary>
+        /// <returns>The byte limit post.</returns>
+        public long GetByteLimitPost()
+        {
+            return byteLimitPost;
+        }
+
+        /// <summary>
+        /// Gets the event store.
+        /// </summary>
+        /// <returns>The event store.</returns>
+        public IStore GetEventStore()
+        {
+            return eventStore;
+        }
+    }
+}
+
