544077aa8ee20d7b3e78e2404588bf89b963600f
Djordje <djordje@twodesperados.com>
2021-11-15 17:25:43 +0100

    added script for building unity package, Demo project adopted to Unity version 2018.4.29f1

--- SnowplowTracker/SnowplowTracker/build.sh
+++ SnowplowTracker/SnowplowTracker/build.sh
@@ -2,12 +2,17 @@
 #define absolute path to game project and snowplow plugin folder
 #build release dll and copy .dll file to Demo, Tests and wokawoka project
 #you can call this script with command " sh build.sh "Release" "ABSOLUTE_PATH_TO_DLL_IN_UNITY_PROJCT" "
-unity_project_plugin_path="/home/djol/Projects/wokawoka/Assets/Plugins/SnowplowAnalytics/SnowplowTracker.dll"
+release_mode="Release"
+if [ ! -z "$1" ]
+then
+    release_mode=$1
+fi
+unity_project_plugin_path="/home/djol/Projects/wokawoka/Assets/Plugins/SnowplowTracker/SnowplowTracker.dll"
 if [ ! -z "$2" ]
 then
     unity_project_plugin_path=$2
 fi
-dotnet build SnowplowTracker.csproj --configuration $1 --framework netstandard2.0
-cp bin/$1/netstandard2.0/SnowplowTracker.dll ../../SnowplowTracker.Demo/Assets/Plugins/SnowplowTracker/SnowplowTracker.dll
-cp bin/$1/netstandard2.0/SnowplowTracker.dll ../../SnowplowTracker.Tests/Assets/Plugins/SnowplowTracker/SnowplowTracker.dll
-cp bin/$1/netstandard2.0/SnowplowTracker.dll $unity_project_plugin_path
+dotnet build SnowplowTracker.csproj --configuration $release_mode --framework netstandard2.0
+cp bin/$release_mode/netstandard2.0/SnowplowTracker.dll ../../SnowplowTracker.Demo/Assets/Plugins/SnowplowTracker/SnowplowTracker.dll
+cp bin/$release_mode/netstandard2.0/SnowplowTracker.dll ../../SnowplowTracker.Tests/Assets/Plugins/SnowplowTracker/SnowplowTracker.dll
+cp bin/$release_mode/netstandard2.0/SnowplowTracker.dll $unity_project_plugin_path
