f3988adf68cf6ace7f0e5ced9adbb982965f36fd
Djordje <djordje@twodesperados.com>
2023-03-21 11:47:08 +0100

    add optional parameters for common events

--- GametunerTracker/GametunerTracker/Wrapper/GametunerUnityTrackerEvents.cs
+++ GametunerTracker/GametunerTracker/Wrapper/GametunerUnityTrackerEvents.cs
@@ -7,7 +7,15 @@ namespace GametunerTracker
     
     public static partial class GametunerUnityTracker
     {
-        public static void LogEventAdStarted(string groupId, string adPlacement, string adPlacementGroup, string adProvider, string adType, int limit, int limitCounter, int durationSeconds, string crosspromo)
+        public static void LogEventAdStarted(string adPlacement,
+                                            string groupId = null,
+                                            string adPlacementGroup = null, 
+                                            string adProvider = null, 
+                                            string adType = null, 
+                                            int limit = int.MinValue, 
+                                            int limitCounter = int.MinValue, 
+                                            int durationSeconds = int.MinValue, 
+                                            string crosspromo = null)
         {
             if (!isInitialized) { 
                 Log.Error("Tracker is not initialized");
@@ -15,20 +23,38 @@ namespace GametunerTracker
             }
 
             Dictionary<string, object> eventData = new Dictionary<string, object>();
-            eventData.Add("group_id", groupId);
             eventData.Add("ad_placement", adPlacement);
-            eventData.Add("ad_placement_group", adPlacementGroup);
-            eventData.Add("ad_provider", adProvider);
-            eventData.Add("ad_type", adType);
-            eventData.Add("limit", limit);
-            eventData.Add("limit_counter", limitCounter);
-            eventData.Add("duration_seconds", durationSeconds);
-            eventData.Add("crosspromo", crosspromo);
+            if (groupId != null)
+                eventData.Add("group_id", groupId);
+            if (adPlacementGroup != null) 
+                eventData.Add("ad_placement_group", adPlacementGroup);
+            if (adProvider != null) 
+                eventData.Add("ad_provider", adProvider);
+            if (adType != null)
+                eventData.Add("ad_type", adType);
+            if (limit != int.MinValue) 
+                eventData.Add("limit", limit);
+            if (limitCounter != int.MinValue) 
+                eventData.Add("limit_counter", limitCounter);
+            if (durationSeconds != int.MinValue) 
+                eventData.Add("duration_seconds", durationSeconds);
+            if (crosspromo != null) 
+                eventData.Add("crosspromo", crosspromo);
 
             LogEvent(EventNames.EVENT_AD_STARTED, Constants.EVENT_AD_STARTED_SCHEMA, eventData, null, 0);
         }
 
-        public static void LogEventAdWatched(string groupId, string ad_placement, string ad_placement_group, string adProvider, string adType, bool rewardClaimed, int limit, int limitCounter, int durationSeconds, int secondsWatched, string crosspromo)
+        public static void LogEventAdWatched(string adPlacement, 
+                                            bool rewardClaimed,
+                                            string groupId = null,                                             
+                                            string adPlacementGroup = null, 
+                                            string adProvider = null, 
+                                            string adType = null, 
+                                            int limit = int.MinValue, 
+                                            int limitCounter = int.MinValue, 
+                                            int durationSeconds = int.MinValue, 
+                                            int secondsWatched = int.MinValue, 
+                                            string crosspromo = null)
         {
             if (!isInitialized) { 
                 Log.Error("Tracker is not initialized");
@@ -36,23 +62,40 @@ namespace GametunerTracker
             }
 
             Dictionary<string, object> eventData = new Dictionary<string, object>();
-            eventData.Add("group_id", groupId);
-            eventData.Add("ad_placement", ad_placement);
-            eventData.Add("ad_placement_group", ad_placement_group);
-            eventData.Add("ad_provider", adProvider);
-            eventData.Add("ad_type", adType);
+            eventData.Add("ad_placement", adPlacement);
             eventData.Add("reward_claimed", rewardClaimed);
-            eventData.Add("limit", limit);
-            eventData.Add("limit_counter", limitCounter);
-            eventData.Add("duration_seconds", durationSeconds);
-            eventData.Add("seconds_watched", secondsWatched);
-            eventData.Add("crosspromo", crosspromo);
+            if (groupId != null)
+                eventData.Add("group_id", groupId);
+            if (adPlacementGroup != null)
+                eventData.Add("ad_placement_group", adPlacementGroup);
+            if (adProvider != null)
+                eventData.Add("ad_provider", adProvider);
+            if (adType != null)
+                eventData.Add("ad_type", adType);
+            if (limit != int.MinValue) 
+                eventData.Add("limit", limit);
+            if (limitCounter != int.MinValue)
+                eventData.Add("limit_counter", limitCounter);
+            if (durationSeconds != int.MinValue) 
+                eventData.Add("duration_seconds", durationSeconds);
+            if (secondsWatched != int.MinValue) 
+                eventData.Add("seconds_watched", secondsWatched);
+            if (crosspromo != null) 
+                eventData.Add("crosspromo", crosspromo);
             
 
             LogEvent(EventNames.EVENT_AD_WATCHED, Constants.EVENT_AD_WATCHED_SCHEMA, eventData, null, 0);
         }
 
-        public static void LogEventCurrencyChange(string groupId, string currency, int stashUpdated, int amountChange, int currencyLimit, int amountWasted, string reason, string gameMode, string screen)
+        public static void LogEventCurrencyChange(string currency, 
+                                                int amountChange,       
+                                                string groupId = null,
+                                                int stashUpdated = int.MinValue, 
+                                                int currencyLimit = int.MinValue, 
+                                                int amountWasted = int.MinValue, 
+                                                string reason = null, 
+                                                string gameMode = null, 
+                                                string screen = null)
         {
             if (!isInitialized) { 
                 Log.Error("Tracker is not initialized");
@@ -60,20 +103,37 @@ namespace GametunerTracker
             }
 
             Dictionary<string, object> eventData = new Dictionary<string, object>();
-            eventData.Add("group_id", groupId);
             eventData.Add("currency", currency);
-            eventData.Add("stash_updated", stashUpdated);
             eventData.Add("amount_change", amountChange);
-            eventData.Add("currency_limit", currencyLimit);
-            eventData.Add("amount_wasted", amountWasted);
-            eventData.Add("reason", reason);
-            eventData.Add("game_mode", gameMode);
-            eventData.Add("screen", screen);
+            if (groupId != null)
+                eventData.Add("group_id", groupId);
+            if (stashUpdated != int.MinValue)
+                eventData.Add("stash_updated", stashUpdated);
+            if (currencyLimit != int.MinValue)
+                eventData.Add("currency_limit", currencyLimit);
+            if (amountWasted != int.MinValue)
+                eventData.Add("amount_wasted", amountWasted);
+            if (reason != null) 
+                eventData.Add("reason", reason);
+            if (gameMode != null)
+                eventData.Add("game_mode", gameMode);
+            if (screen != null)
+                eventData.Add("screen", screen);
 
             LogEvent(EventNames.EVENT_CURRENCY_CHANGE, Constants.EVENT_CURRENCY_CHANGE_SCHEMA, eventData, null, 0);
         }
 
-        public static void LogEventPurchaseInitiated(string paymentProvider, string packageName, string packageContents, int premiumCurrency, double price, string priceCurrency, float priceUSD, string shopPlacement, string gameMode, string screen, string groupId)
+        public static void LogEventPurchaseInitiated(string packageName, 
+                                                    string paymentProvider = null, 
+                                                    string packageContents = null, 
+                                                    int premiumCurrency = int.MinValue,   
+                                                    double price = double.MinValue, 
+                                                    string priceCurrency = null,                                                   
+                                                    float priceUSD = float.MinValue, 
+                                                    string shopPlacement = null, 
+                                                    string gameMode = null, 
+                                                    string screen = null, 
+                                                    string groupId = null)
         {
             if (!isInitialized) { 
                 Log.Error("Tracker is not initialized");
@@ -81,23 +141,50 @@ namespace GametunerTracker
             }
 
             Dictionary<string, object> eventData = new Dictionary<string, object>();
-            eventData.Add("payment_provider", paymentProvider);
             eventData.Add("package_name", packageName);
-            eventData.Add("package_contents", packageContents);
-            eventData.Add("premium_currency", premiumCurrency);
-            eventData.Add("price", price);
-            eventData.Add("price_currency", priceCurrency);
-            eventData.Add("price_usd", priceUSD);            
-            eventData.Add("game_mode", gameMode);
-            eventData.Add("shop_placement", shopPlacement);
-            eventData.Add("screen", shopPlacement);
-            eventData.Add("group_id", groupId);
+            if (paymentProvider != null)
+                eventData.Add("payment_provider", paymentProvider);
+            if (packageContents != null)
+                eventData.Add("package_contents", packageContents);
+            if (premiumCurrency != int.MinValue)
+                eventData.Add("premium_currency", premiumCurrency);
+            if (price != double.MinValue)
+                eventData.Add("price", price);
+            if (priceCurrency != null)
+                eventData.Add("price_currency", priceCurrency);
+            if (priceUSD != float.MinValue)
+                eventData.Add("price_usd", priceUSD);
+            if (shopPlacement != null)
+                eventData.Add("shop_placement", shopPlacement);
+            if (gameMode != null)
+                eventData.Add("game_mode", gameMode);
+            if (screen != null)
+                eventData.Add("screen", screen);
+            if (groupId != null)
+                eventData.Add("group_id", groupId);
 
             LogEvent(EventNames.EVENT_PURCHASE_INITIATED, Constants.EVENT_PURCHASE_INITIATED_SCHEMA, eventData, null, 0);
         }
 
         //Create method to log event Purchase with parameters
-        public static void LogEventPurchase(string transactionId, string paymentProvider, string payload, string packageName, string packageContents, int premiumCurrency, double price, string priceCurrency, double priceUsd, double paidAmount, string paidCurrency, double paidUsd, string gameMode, string shopPlacement, string screen, string transactionCountryCode, string group_id, Dictionary<string, int> packageItems)
+        public static void LogEventPurchase(string packageName, 
+                                            double paidAmount, 
+                                            string paidCurrency, 
+                                            string transactionId = null, 
+                                            string paymentProvider = null, 
+                                            string payload = null,  
+                                            string packageContents = null, 
+                                            int premiumCurrency = int.MinValue, 
+                                            double price = double.MinValue, 
+                                            string priceCurrency = null, 
+                                            double priceUsd = double.MinValue,                                            
+                                            double paidUsd = double.MinValue, 
+                                            string gameMode = null, 
+                                            string shopPlacement = null, 
+                                            string screen = null, 
+                                            string transactionCountryCode = null, 
+                                            string groupId = null, 
+                                            Dictionary<string, int> packageItems = null)
         {
             if (!isInitialized) { 
                 Log.Error("Tracker is not initialized");
@@ -105,24 +192,39 @@ namespace GametunerTracker
             }
 
             Dictionary<string, object> eventData = new Dictionary<string, object>();
-            eventData.Add("transaction_id", transactionId);
-            eventData.Add("payment_provider", paymentProvider);
-            eventData.Add("payload", payload);
             eventData.Add("package_name", packageName);
-            eventData.Add("package_contents", packageContents);
-            eventData.Add("premium_currency", premiumCurrency);
-            eventData.Add("price", price);
-            eventData.Add("price_currency", priceCurrency);
-            eventData.Add("price_usd", priceUsd);
             eventData.Add("paid_amount", paidAmount);
             eventData.Add("paid_currency", paidCurrency);
-            eventData.Add("paid_usd", paidUsd);
-            eventData.Add("game_mode", gameMode);
-            eventData.Add("shop_placement", shopPlacement);
-            eventData.Add("screen", screen);
-            eventData.Add("transaction_country_code", transactionCountryCode);
-            eventData.Add("group_id", group_id);
-            eventData.Add("package_items", packageItems);
+            if (transactionId != null)
+                eventData.Add("transaction_id", transactionId);
+            if (paymentProvider != null)
+                eventData.Add("payment_provider", paymentProvider);
+            if (payload != null)
+                eventData.Add("payload", payload);
+            if (packageContents != null)
+                eventData.Add("package_contents", packageContents);
+            if (premiumCurrency != int.MinValue)   
+                eventData.Add("premium_currency", premiumCurrency);
+            if (price != double.MinValue)
+                eventData.Add("price", price);
+            if (priceCurrency != null)  
+                eventData.Add("price_currency", priceCurrency);
+            if (priceUsd != double.MinValue)
+                eventData.Add("price_usd", priceUsd);
+            if (paidUsd != double.MinValue)
+                eventData.Add("paid_usd", paidUsd);
+            if (gameMode != null)
+                eventData.Add("game_mode", gameMode);
+            if (shopPlacement != null)
+                eventData.Add("shop_placement", shopPlacement);
+            if (screen != null)
+                eventData.Add("screen", screen);
+            if (transactionCountryCode != null)
+                eventData.Add("transaction_country_code", transactionCountryCode);
+            if (groupId != null)    
+                eventData.Add("group_id", groupId);
+            if (packageItems != null)
+                eventData.Add("package_items", packageItems);
 
             LogEvent(EventNames.EVENT_PURCHASE, Constants.EVENT_PURCHASE_SCHEMA, eventData, null, 0);
         }
